0d6943e9cd5a3767e0afb320234fbaa45b728847
Rheydskey <matgag02@gmail.com>
2022-08-30 22:09:37 +0200

    Add more way to move between feeds

--- lib/page.dart
+++ lib/page.dart
@@ -6,6 +6,47 @@ import 'package:ttrss_client/class/feed.dart';
 import 'package:ttrss_client/class/session.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+class FeedPage {
+  List<Articles>? articles;
+
+  void setArticles(List<Articles> listArticles) {
+    articles ??= listArticles;
+  }
+
+  Widget toWidget(BuildContext context) {
+    if (articles == null) {
+      return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [Center(child: CircularProgressIndicator())]);
+    }
+
+    return ListView(
+        children: articles!
+            .map((e) => articlesCard(e.title, e.link, context))
+            .toList());
+  }
+}
+
+class FeedPages {
+  List<FeedPage> pages = [];
+
+  void setArticles(int index, List<Articles> articles) {
+    getPage(index)?.setArticles(articles);
+  }
+
+  void addPage(FeedPage page) {
+    pages.add(page);
+  }
+
+  FeedPage? getPage(int index) {
+    return pages[index];
+  }
+
+  List<Widget> getWidgets(BuildContext context) {
+    return pages.toList().map((e) => e.toWidget(context)).toList();
+  }
+}
+
 class Logged extends StatefulWidget {
   final Session session;
 
@@ -24,16 +65,17 @@ class Logged extends StatefulWidget {
 
 class LoggedState extends State<Logged> with SingleTickerProviderStateMixin {
   late Session session = widget.getSession();
+  final PageController _pageController = PageController(keepPage: false);
   List<Feed> feeds = [];
   List<Widget> tabFeed = [];
+  FeedPages pageFeed = FeedPages();
   StreamSubscription<List>? feedLoading;
-  Widget? viewArticles;
+  List<Widget> viewArticles = [];
   TabController? _tabController;
 
   @override
   void initState() {
     super.initState();
-
     refresh();
   }
 
@@ -41,6 +83,7 @@ class LoggedState extends State<Logged> with SingleTickerProviderStateMixin {
   void dispose() {
     super.dispose();
     _tabController?.dispose();
+    _pageController.dispose();
   }
 
   Future<void> refresh() async {
@@ -49,36 +92,25 @@ class LoggedState extends State<Logged> with SingleTickerProviderStateMixin {
     });
   }
 
-  void setArticles(List<Articles> articles) {
-    setState(() {
-      List<Widget> articlesList = [];
-      for (var article in articles) {
-        articlesList.add(articlesCard(article.title, article.link, context));
-      }
-
-      viewArticles = ListView(
-        children: articlesList,
-      );
-    });
-  }
-
-  void setArticlesOfFeedIndex(int index) {
-    setState(() {
-      viewArticles = Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [Center(child: CircularProgressIndicator())]);
-
-      feedLoading =
-          session.getArticles(feeds[index]).asStream().listen(setArticles);
-    });
-  }
-
-  void setArticlesOfCurrentIndex() {
-    if (feedLoading != null) {
-      feedLoading?.cancel();
+  void tabPageListener(String from, int index) {
+    if (from == "tab") {
+      _pageController.jumpToPage(index);
     }
 
-    setArticlesOfFeedIndex(_tabController!.index);
+    if (from == "page") {
+      _tabController?.animateTo(index);
+    }
+
+    setArticles(index);
+  }
+
+  void setArticles(int index) {
+    session
+        .getArticles(feeds[index])
+        .then((articles) => pageFeed.setArticles(index, articles))
+        .then((_) => setState(() {
+              viewArticles = pageFeed.getWidgets(context);
+            }));
   }
 
   void setFeeds(List<Feed>? gettedfeeds) {
@@ -90,14 +122,68 @@ class LoggedState extends State<Logged> with SingleTickerProviderStateMixin {
       for (var feed in gettedfeeds) {
         feeds.add(feed);
         tabFeed.add(Tab(child: Text(feed.title)));
+        pageFeed.addPage(FeedPage());
       }
+      viewArticles = pageFeed.getWidgets(context);
 
       _tabController = TabController(length: tabFeed.length, vsync: this);
 
-      _tabController?.addListener(setArticlesOfCurrentIndex);
+      _tabController
+          ?.addListener(() => tabPageListener("tab", _tabController!.index));
 
-      setArticlesOfFeedIndex(0);
+      setArticles(0);
     });
+  }
+
+  PreferredSizeWidget? buildTabBar() {
+    return _tabController!.length == 0
+        ? null
+        : TabBar(
+            isScrollable: true,
+            controller: _tabController,
+            unselectedLabelColor: Colors.white.withOpacity(0.3),
+            indicatorColor: Colors.white,
+            tabs: tabFeed,
+          );
+  }
+
+  Widget? buildDrawer() {
+    return Drawer(
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+                height: MediaQuery.of(context).padding.top + 30,
+                child: const DrawerHeader(
+                    padding: EdgeInsets.zero,
+                    margin: EdgeInsets.zero,
+                    child: Text("TTRss Client",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20.0)))),
+            Expanded(
+                child: ListView.builder(
+              padding: EdgeInsets.zero,
+              primary: false,
+              itemCount: feeds.length,
+              itemBuilder: (BuildContext context, index) => Container(
+                margin: const EdgeInsets.only(top: .0, bottom: 8.0),
+                child: TextButton(
+                    onPressed: () {
+                      _tabController?.index = index;
+                      Navigator.pop(context);
+                    },
+                    child: SizedBox(
+                        width: double.maxFinite,
+                        child: Text(
+                          feeds[index].title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ))),
+              ),
+            ))
+          ]),
+    );
   }
 
   @override
@@ -117,23 +203,31 @@ class LoggedState extends State<Logged> with SingleTickerProviderStateMixin {
       );
     }
     return Scaffold(
+        drawer: buildDrawer(),
         backgroundColor: Colors.grey.shade200,
         appBar: AppBar(
+          leading: Builder(builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu, color: Colors.white),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          }),
           title:
               const Text("TinyTinyRss", style: TextStyle(color: Colors.white)),
           centerTitle: true,
           backgroundColor: Colors.purple.shade900,
-          bottom: _tabController!.length == 0
-              ? null
-              : TabBar(
-                  isScrollable: true,
-                  controller: _tabController,
-                  unselectedLabelColor: Colors.white.withOpacity(0.3),
-                  indicatorColor: Colors.white,
-                  tabs: tabFeed,
-                ),
+          bottom: buildTabBar(),
         ),
-        body: viewArticles);
+        body: PageView(
+          controller: _pageController,
+          children: viewArticles,
+          onPageChanged: (index) => tabPageListener(
+            "page",
+            index,
+          ),
+        ));
   }
 }
 
